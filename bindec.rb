7f3a57841110c9a955cad975356c6d1e88377682
Slav <slav999@ukr.net>
2016-08-25 15:54:41 +0300

    updates

--- bindec.rb
+++ bindec.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 unless (ARGV.size == 2 && ARGV[0] =~ /0|^[1-9][\d_]*$/ && ARGV[1] =~ /^(0b)?[01]+[01_]*$/)
-  abort "USAGE: ./bindec.rb DECIMAL BINARY"
+  abort "USAGE: #{$0} DECIMAL BINARY"
 end
 
 decimal = ARGV[0].tr('_','')
@@ -10,12 +10,12 @@ binary  = ARGV[1].gsub(/^0b|_/,'')
 
 def dec2bin decimal # => 10.to_s(2) == 1010
     d = decimal.to_i
-    bin_arr = (d == 0) ? [d] : []
+    bin_arr = (d == 0) ? [0] : []
     while d > 0 do
-      bin_arr.unshift (d % 2 == 0) ? 0 : 1
+      bin_arr.unshift d % 2
       d = d / 2
     end
-  puts "Decimal: #{decimal} -> #{bin_arr.join}"
+  return bin_arr.join
 end
 
 def bin2dec binary  # => 1010.to_s.to_i(2) == 10
@@ -25,8 +25,8 @@ def bin2dec binary  # => 1010.to_s.to_i(2) == 10
           power -= 1
           dec += d.to_i*2**(power)
       end
-  puts "Binary : #{binary} -> #{dec}"
+  return dec
 end
 
-dec2bin decimal
-bin2dec binary
+puts "Decimal digit #{decimal} -> #{dec2bin(decimal)}"
+puts "Binary  digit #{binary} -> #{bin2dec(binary)}"
