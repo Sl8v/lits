a03d460c2287dc6ce736c921e8356797009eb442
Slav <Slav999@ukr.net>
2016-09-20 00:32:23 +0300

    L9-10: added seeds & db-reader

---
+++ lesson10/db_connector.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+require 'mysql2'
+
+client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "toor", :database => "figures")
+
+
+results = client.query("SELECT figures.name , formulas.content from figures, formulas where formulas.figure_id = figures.id")
+
+results.each do |row|
+  # conveniently, row is a hash
+  # the keys are the fields, as you'd expect
+  # the values are pre-built ruby primitives mapped from their corresponding field types in MySQL
+  puts "#{row["name"]} : #{row["content"]}" 
+end
