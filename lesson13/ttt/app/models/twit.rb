99dac1cb47b677e1580bca96db42fb6076b64b08
Slav <Slav999@ukr.net>
2016-10-12 01:14:54 +0300

    Comments added

--- lesson13/ttt/app/models/twit.rb
+++ lesson13/ttt/app/models/twit.rb
@@ -1,3 +1,8 @@
 class Twit < ActiveRecord::Base
   belongs_to :user
+
+  validates :user_id, :presence => true
+                      # :content => { :minimum => 5 }
+
+  has_many :comments, dependent: :destroy
 end
--- lesson13/ttt/app/models/user.rb
+++ lesson13/ttt/app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 
-  has_many :twits
-  
+  has_many :twits, dependent: :destroy
+
+  validates :nickname, uniqueness: true, length: { in: 3..20 }
+  validates :email, :presence => true
+
+  has_many :comments, dependent: :destroy
+
+
 end
