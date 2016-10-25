834db8ca85a2ee971ac4a1c076f5b8555c7cba1f
Slav <slav999@ukr.net>
2016-10-25 17:43:16 +0300

    schema

--- lesson13/ttt/db/schema.rb
+++ lesson13/ttt/db/schema.rb
@@ -26,9 +26,12 @@ ActiveRecord::Schema.define(version: 20161022120349) do
 
   create_table "tags", force: :cascade do |t|
     t.string   "name"
+    t.integer  "twit_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "tags", ["twit_id"], name: "index_tags_on_twit_id"
 
   create_table "twit_tags", force: :cascade do |t|
     t.integer  "twit_id"
