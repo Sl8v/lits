4ec67059eba1d97607989e99d92746d300837e66
Slav <Slav999@ukr.net>
2016-10-26 16:37:55 +0300

    Tests for user

--- lesson13/ttt/test/models/user_test.rb
+++ lesson13/ttt/test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
+  test 'the truth' do
     assert true
   end
 
@@ -10,4 +10,49 @@ class UserTest < ActiveSupport::TestCase
     assert_not user.save
   end
 
+  ## each new test
+  setup do
+    @user = users(:two)
+  end
+
+  # after each test
+  teardown do
+    Rails.cache.clear
+  end
+
+  test 'user is saved' do
+    user = User.new(nickname: 'test', email: 'a@2.com')
+    assert user.save!
+  end
+
+  test 'should not save user without nickname' do
+    user = User.new(email: 'werw@werw.com')
+    # assert_raises(ActiveRecord::StatementInvalid, SQLite3::ConstraintException){ user.save }
+    assert_not user.save
+  end
+
+  test 'should update user' do
+    @user.nickname = 'updated'
+    @user.save!
+    @user.reload
+    assert_equal 'updated', @user.nickname
+  end
+
+  test 'should not update user' do
+    @user.nickname = ''
+    assert_raises("Nickname can't be blank") { user.save }
+    # assert_not @user.save!
+  end
+
+  test 'should destroy user' do
+    assert_difference('User.count', -1) do
+      @user.destroy
+    end
+  end
+
+  test 'should not destroy user' do
+    assert_difference('User.count', -1) do
+      User.find(100).destroy
+    end
+  end
 end
