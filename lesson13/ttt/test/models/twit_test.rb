9bf6cf8f9be2e21830af56620746bc336a6ce13d
Slav <Slav999@ukr.net>
2016-10-27 22:38:47 +0300

    tests: simplecov added

--- lesson13/ttt/test/models/twit_test.rb
+++ lesson13/ttt/test/models/twit_test.rb
@@ -16,7 +16,11 @@ class TwitTest < ActiveSupport::TestCase
 
   test 'should not save twit without content' do
     twit = Twit.new(user_id: 1)
-    # assert_raises(ActiveRecord::StatementInvalid, SQLite3::ConstraintException){ twit.save }
+    assert_not twit.save
+  end
+
+  test 'should not save twit with same id' do
+    twit = Twit.new(user_id: @twit.user_id, content: 'ddd')
     assert_not twit.save
   end
 
